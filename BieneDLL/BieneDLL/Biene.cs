50191e2bd82d4aace134f155d0601eeb50139cca
programmiertier <zimmermannveronique@protonmail.com>
2018-01-29 10:32:14 +0100

    C# Aufbau02

---
+++ BieneDLL/BieneDLL/Biene.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace BieneDLL
+{
+    public class Biene
+    {
+        // Klasseneigenschaft
+        private static string _farbe = "gelb-schwarz gestreift";
+
+        // Objekteigenschaften = properties
+        // schreibender Zugriff: private        lesender Zugriff: öffentlich
+        public int alter { get; private set; }
+        public double gewicht { get; private set; }
+        public egeschlecht geschlecht { get; private set; }
+
+        // Konstruktor
+        public Biene() { }
+        public Biene(int alter, double gewicht, egeschlecht was)
+        {
+            this.alter = alter;
+            this.gewicht = gewicht;
+            this.geschlecht = was;
+            getFarbe();
+        }
+
+        // die Methoden zum ändern der Objekteigenschaften
+        // wenn eine Biene Geburtstag hat, dann wird sie genau ein Jahr älter
+        public void feiernGeburtstag()
+        {
+            this.alter++;
+        }
+
+        // fliegen ändert das Gewicht
+        public string fliegen(double strecke)
+        {
+            // pro 100m 1g weniger
+            double aenderung = strecke / 100;   // 1km wären 10g weniger
+            if (this.gewicht >= aenderung)
+            {
+                this.gewicht -= aenderung;
+                return "Ich hab es geschafft";
+            }
+            else
+            {
+                return "Das wird nix";
+            }
+        }
+
+        // futtern ändert auch das Gewicht
+        public string futtern(double gramm)
+        {
+            // jedes Gramm Honig, führt dazu, dass sie 0,5g zunimmt
+            double aenderung = gramm / 2;
+            this.gewicht += aenderung;
+            return "Jetzt bin ich satt, ich rolle in meine Wabe";
+        }
+
+        // Geschlechtsumwandlung
+        public string umwandlungGeschlecht()
+        {
+            // es wird immer das Gegenteil vom aktuellen Geschlecht
+            if (this.geschlecht == egeschlecht.maennlich)
+            {
+                this.geschlecht = egeschlecht.weiblich;
+                return "Nun bin ich weiblich";
+            }
+            else
+            {
+                this.geschlecht = egeschlecht.maennlich;
+                return "Nun bin ich männlich";
+            }
+        }
+
+        // Klassenmethode
+        public static String getFarbe()
+        {
+            return _farbe;
+        }
+
+        // für das Geschlecht eine Auflistung möglicher Werte
+        public enum egeschlecht
+        {
+            maennlich, weiblich
+        }
+
+        // Biene soll Blume bestäuben
+        public String gehbestaeuben(Blume welche)
+        {
+            String result = welche.bestaeuben();
+            return "Auftrag ausgeführt.Blume sagt:" + result;
+        }
+
+        public override string ToString()
+        {
+            return alter + "|" + gewicht + "|" + geschlecht + "|" + _farbe;
+        }
+    }
+}
---
+++ BieneDLL/BieneDLL/Blume.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BieneDLL
+{
+    public class Blume
+    {
+        public String farbe { get; private set; }
+        public String art { get; private set; }
+
+        public Blume(String farbe, String art)
+        {
+            this.farbe = farbe;
+            this.art = art;
+        }
+
+        // diese Methode darf nur von der Biene aufgerufen werden
+        // internal heißt, Zugriff nur innerhalb der gleichen dll
+        internal String bestaeuben()
+        {
+            return "Die Biene war da";
+        }
+    }
+}
