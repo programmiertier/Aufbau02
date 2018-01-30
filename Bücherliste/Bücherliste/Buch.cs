1bd404e53c12cc54bf1ed1c5869ad33616bdc30a
programmiertier <zimmermannveronique@protonmail.com>
2018-01-30 08:19:27 +0100

    Bücherliste

---
+++ Bücherliste/Bücherliste/Buch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bücherliste
+{
+    class Buch
+    {
+        public String autor { get; private set; }
+        public String titel { get; private set; }
+        public int auflage { get; private set; }
+        public double preis { get; private set; }
+
+
+        public Buch(String autor, String titel, int auflage, double preis)
+        {
+            this.autor = autor;
+            this.titel = titel;
+            this.auflage = auflage;
+            this.preis = preis;
+        }
+
+        // gleicher Autor, gleicher Titel -- Die Bücher sind identisch
+        public override bool Equals(object obj)
+        {
+            if (obj is Buch)
+            {
+                Buch vgl = (Buch)obj;
+                if(this.autor == vgl.autor && this.titel == vgl.titel)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        // gleicher Autor -- die Bücher könnten identisch sein
+        public override int GetHashCode()
+        {
+            return this.autor.Length;
+        }
+    }
+}
