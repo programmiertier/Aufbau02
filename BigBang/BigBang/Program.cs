df511e29a99c5eb4018574ec88563b74407845f6
programmiertier <zimmermannveronique@protonmail.com>
2018-02-02 08:47:54 +0100

    BigBang

---
+++ BigBang/BigBang/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace BigBang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Schalter button = new Schalter();
+            button.hinzufügen(new Licht());
+            button.hinzufügen(new Spinne());
+            button.ein();
+
+            button.aus();
+
+            ReadLine();
+        }
+    }
+}
---
+++ BigBang/BigBang/Schalter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigBang
+{
+    class Schalter
+    {
+        public List<Element> was{ get; private set; }
+
+        public Schalter()
+        {
+            was = new List<Element>();
+        }
+
+        public void hinzufügen(Element was)
+        {
+            this.was.Add(was);
+        }
+
+        public void ein()
+        {
+            foreach (Element e in was)
+            {
+                e.einschalten();
+            }
+        }
+
+        public void aus()
+        {
+            foreach (Element e in was)
+            {
+                e.ausschalten();
+            }
+        }
+    }
+}
