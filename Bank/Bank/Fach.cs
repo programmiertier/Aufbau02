db6b1410c03e426309e880d433b4551702afcff1
programmiertier <zimmermannveronique@protonmail.com>
2018-02-05 10:41:01 +0100

    Bankschließfach switch case

---
+++ Bank/Bank/Fach.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class Schließfach
+    {
+        private static int lfdNummer = 0;
+
+        public int nummer { get; private set; }
+
+        public bool belegt = false;
+
+        public List<String> inhalt { get; private set; }
+        
+        public Schließfach()
+        {
+            inhalt = new List<String>();
+            belegt = false;
+            nummer = ++lfdNummer;
+        }
+    }
+}
