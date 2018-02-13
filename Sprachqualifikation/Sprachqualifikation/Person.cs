a6b417080e48d89f421e51e072a18d0196cb838b
VeroniqueZimmermann <zimmermannveronique@protonmail.com>
2018-02-13 10:24:11 +0100

    Sprachqualifikationen SQL

---
+++ Sprachqualifikation/Sprachqualifikation/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprachqualifikation
+{
+    public class Person
+    {
+        public String name { get; set; }
+
+        public String vorname { get; set; }
+
+        public String persNr { get; set; }
+    }
+}
--- Sprachqualifikation/Sprachqualifikation/Program.cs
+++ Sprachqualifikation/Sprachqualifikation/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace Sprachqualifikation
 {
@@ -10,6 +11,24 @@ namespace Sprachqualifikation
     {
         static void Main(string[] args)
         {
+            AbfragePool pool = null;
+            try
+            {
+                pool = new AbfragePool();
+            }
+            catch (Exception e)
+            {
+                WriteLine("Keine Verbindung zur DB");
+                return;
+            }
+
+            WriteLine("Liste aller Personen");
+            foreach( Person p in pool.getPersonen())
+            {
+
+            }
+
+            ReadLine();
         }
     }
 }
