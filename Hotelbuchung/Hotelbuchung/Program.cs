1f12d35851de2f18a4e63fefbb19edf8e0bd4fc9
programmiertier <zimmermannveronique@protonmail.com>
2018-02-14 14:21:16 +0100

    Sinnfrei

---
+++ Hotelbuchung/Hotelbuchung/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BiboHotel;
+
+namespace Hotelbuchung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Buchung buu = new Buchung();
+            Console.WriteLine(buu);
+
+            Hotel ho = new Hotel();
+            Console.WriteLine(ho.ausgebenAlle());
+            Console.WriteLine(ho.ausgebenNamen("fr") + "\t" + ho.ausgebenZimmer().ToString() + "\t" + ho.ausgebenZimmerBuchung(1));
+            Console.WriteLine(buu);
+            
+
+        }
+    }
+}
