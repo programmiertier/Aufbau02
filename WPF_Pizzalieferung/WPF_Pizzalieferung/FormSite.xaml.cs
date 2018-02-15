2bea15ad0ed9aa0f8e849b1d516c0224eb4bc1ed
vzim <veronique@programmiertier.de>
2018-02-15 11:34:14 +0100

    WPF Styles

--- WPF_Pizzalieferung/WPF_Pizzalieferung/FormSite.xaml.cs
+++ WPF_Pizzalieferung/WPF_Pizzalieferung/FormSite.xaml.cs
@@ -22,5 +22,16 @@ namespace WPF_Pizzalieferung
         {
             InitializeComponent();
         }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            // Farbverlauf des Grids soll sich ändern
+            // Ich könnte auf dei Eigenschaft des Grids zugreifen
+            // gridNo1.Background= ...
+
+            // eine Ressource wird geändert
+            this.Resources["styleVerlauf"] = new LinearGradientBrush(Colors.Orchid, Colors.OrangeRed, 45);
+
+        }
     }
 }
