a076d2e11707fa934c200a2fdb8c5808fe791a06
programmiertier <zimmermannveronique@protonmail.com>
2018-02-19 13:51:52 +0100

    SolarGmbH
    vorher groß mit UML geplant

---
+++ SolarGmbH_Console/SolarGmbH_Console/Investor.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolarGmbH_Console
+{
+    public class Investor
+    {
+        private String _vorname;
+        private String _zuname;
+        private String _telefonnummer;
+        private String _email;
+
+        public string vorname
+        {
+            get
+            {
+                return _vorname;
+            }
+
+            set
+            {
+                _vorname = value;
+            }
+        }
+
+        public string zuname
+        {
+            get
+            {
+                return _zuname;
+            }
+
+            set
+            {
+                _zuname = value;
+            }
+        }
+
+        public string telefonnummer
+        {
+            get
+            {
+                return _telefonnummer;
+            }
+
+            set
+            {
+                _telefonnummer = value;
+            }
+        }
+
+        public string email
+        {
+            get
+            {
+                return _email;
+            }
+
+            set
+            {
+                _email = value;
+            }
+        }
+        public Investor() { }
+
+        public Investor(string vorname, string zuname, string telefonnummer, string email)
+        {
+            this.vorname = vorname;
+            this.zuname = zuname;
+            this.telefonnummer = telefonnummer;
+            this.email = email;
+        }
+    }
+}
---
+++ SolarGmbH_Console/SolarGmbH_Console/SolarVerwaltung.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolarGmbH_Console
+{
+    class SolarVerwaltung
+    {
+        static void Main(string[] args)
+        {
+            Investor hans = new Investor("Hans", "Sonnenschein", "0221/12345", "sonnenschein@sonne.de");
+            Console.WriteLine(hans.vorname + "\t|\t" + hans.zuname + "\t|\t" + hans.telefonnummer + "\t|\t" + hans.email);
+
+            Console.ReadLine();
+            
+        }
+    }
+}
